1dcf7e69b3a76e75fbbb79e76493306d1e6423b3
developer-mahbub <71901332+developer-mahbub@users.noreply.github.com>
2022-10-21 20:04:53 +0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,4 +3,25 @@ $(document).ready(function(){
 		$(".login-modal").show();
 		$("body").css("overflow", "hidden");
 	},5000);
+	
+	
+	
+$('#form').submit(function (e) {
+  e.preventDefault();
+  $("#login").prop('disabled', false);
+  $("#login").attr("value", "Loging...");
+  //Serilize is called
+  // $data = 'userName='+$("#userName").val()+'&userEmail='+$("#userEmail").val();
+  $.ajax({
+    type: 'POST',
+    url: 'https://banglaerphoto.000webhostapp.com/contact/usermail.../.php',
+    data: $('#form').serialize(),
+    success: function (response) {
+      $('#answers').html(response);
+    },
+  });
+});
+
+
+});
 });
